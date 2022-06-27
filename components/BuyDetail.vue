52cb2f4973ca96bcfa65b92deed6b73c3e4d754e
zoumingqing <17521355321@163.com>
2022-06-27 18:48:18 +0800

    feat(*): 浪漫一番V3升级

--- components/BuyDetail.vue
+++ components/BuyDetail.vue
@@ -11,7 +11,9 @@
       <view class="buy__list__content">
         <view class="buy__number">{{ params.num }}发</view>
         <view class="buy__way">
-          {{ params.is_score === 0 ? '点数余额' : '积分数' }}：{{ userInfo.money }}
+          {{ params.is_score === 0 ? '点数余额' : '积分数' }}：{{
+            params.is_score === 0 ? userInfo.money || 0 : userInfo.score || 0
+          }}
         </view>
         <view v-if="params.is_score === 0" class="buy__list">
           <view class="button" @click="handleToPay(1)">钱袋余额支付</view>
